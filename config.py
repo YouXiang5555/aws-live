ddbeff2cc3333cea3d0552dc033775515538b858
lowchoonkeat <91364310+lowchoonkeat@users.noreply.github.com>
2023-04-13 17:38:07 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-customhost = "database-1.csmpbywl81wh.us-east-1.rds.amazonaws.com"
+customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "lowchoonkeat-employee"
 customregion = "us-east-1"
+
